501a7e400292e6177eb2b470dfb42d342e7fe162
Momchil Tsekov <mtsekov@zohomail.eu>
2023-05-16 00:08:00 +0300

    NoteList: update

--- src/components/NoteList.tsx
+++ src/components/NoteList.tsx
@@ -66,11 +66,6 @@ const NoteList = ({
       <button onClick={handleClose}>I Accept</button>
     </div>
   );
-  const modal = (
-    <Modal onClose={handleClose} actionBar={actionBar}>
-      <p>Here is important agreement for you to accept</p>
-    </Modal>
-  );
 
   return (
     <>
@@ -255,17 +250,14 @@ const NoteList = ({
 
       {/* Edit Modal */}
 
-      {/* <EditTagsModal
+      <EditTagsModal
         show={showModal}
         setShowModal={() => setShowModal(false)}
         availableTags={availableTags}
         onUpdateTag={onUpdateTag}
         onDeleteTag={onDeleteTag}
-      /> */}
+      />
 
-      <button onClick={handleClick}>Opne Modal</button>
-      {showModal ? modal : null}
-      <div className="modal-container"></div>
       {/* END EDIT MODAL */}
     </>
   );
@@ -282,9 +274,7 @@ function EditTagsModal(
         <>
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
-              {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                {/*header*/}
                 <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
                   <h3 className="text-3xl font-semibold">Edit Tags</h3>
                   <button
@@ -296,7 +286,7 @@ function EditTagsModal(
                     </span>
                   </button>
                 </div>
-                {/*body*/}
+
                 <div className="relative p-6 flex-auto">
                   <p className="my-4 text-slate-500 text-lg leading-relaxed">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -305,7 +295,7 @@ function EditTagsModal(
                     tempora aliquid, debitis obcaecati. Minus?
                   </p>
                 </div>
-                {/*footer*/}
+
                 <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
                   <button
                     className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
